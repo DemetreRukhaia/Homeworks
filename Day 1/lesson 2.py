efb0017268dc13987cb60f12b6e5e6a2aa10dd9b
DemetreRukhaia <demetre.rukhaia@gmail.com>
2023-06-27 15:55:04 +0400

    first commit

---
+++ Day 1/lesson 2.py
@@ -0,0 +1,10 @@
+name = "Demetre" #string ტიპის ცვლადი (str)
+surname = "rukhaia"
+age = 15 #integer ტიპის ცვლადი (int)
+height = 1.87 #float ტიპის ცვლადი (ათწილადი)
+
+#boolean (bool) ტიპის ცვლადი
+learning_programming = True
+knows_programming = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+print("მე მქვია" + " " + name + " " + surname + ". " + "მე ვარ" + " " + str(age) + " წლის და ვარ" + " " + str(height) + " სიმაღლეში.")
